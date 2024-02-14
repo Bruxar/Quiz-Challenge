d5d187f196e01e609374ea0a6d1626de271d89e3
Bruxar <larancibia@utem.cl>
2024-02-14 19:52:46 -0300

    Update score layout and restart functionality

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Score = ({ score, totalQuestions }) => {
+  const navigate = useNavigate(); // Obtiene la función de navegación
+
   // Calcula el porcentaje de respuestas correctas
   const percentage = (score / totalQuestions) * 100;
 
@@ -14,6 +17,11 @@ const Score = ({ score, totalQuestions }) => {
     message = "You're the professor now!";
   }
 
+  // Función para manejar el reinicio del quiz
+  const handleRestart = () => {
+    navigate('/Quiz-Challenge/'); // Navega a la página de inicio del quiz
+  };
+
   return (
     <div className='contenedor container-score'>
       <div className='score'>
@@ -21,7 +29,8 @@ const Score = ({ score, totalQuestions }) => {
         <p><span>{score}</span> / {totalQuestions}</p>
       </div>
       <p className='message-score'>{message}</p>
-      <a href="/Quiz-Challenge/">Restart</a>
+      {/* Usa la función navigate para reiniciar el quiz */}
+      <button onClick={handleRestart}>Restart</button>
     </div>
   );
 };
